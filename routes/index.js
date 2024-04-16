f924ebd6275894b1d18bba71cdb33c774a9db118
Abhijeet Nigam <basant.abhijeet2003@gmail.com>
2024-04-16 13:24:37 +0530

    Changes Done

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,24 @@
-const express = require('express');
-const { registerUser, loginUser, getUsers } = require('../userController');
-const { userRegisterValidate, userLoginValidate } = require('../utils/userValiadation');
-const { ensureAuthenticated } = require('../utils/auth');
+const express = require("express");
+const {
+  registerUser,
+  loginUser,
+  getUsers,
+  fillTender,
+} = require("../userController");
+const {
+  userRegisterValidate,
+  userLoginValidate,
+} = require("../utils/userValiadation");
+const { ensureAuthenticated } = require("../utils/auth");
 const routes = express.Router();
 
+routes.post("/register", userRegisterValidate, registerUser);
 
+routes.post("/login", userLoginValidate, loginUser);
 
-routes.post('/register', userRegisterValidate ,registerUser);
+routes.get("/users", ensureAuthenticated, getUsers);
 
-routes.post('/login', userLoginValidate, loginUser);
-
-routes.get('/users', ensureAuthenticated, getUsers);
-
-
+routes.post("/", fillTender);
+routes.put("/", updateTender);
+routes.get("/", getTender);
 module.exports = routes;
